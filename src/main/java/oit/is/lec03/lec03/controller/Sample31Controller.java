46f168d7a867bf8124d2971b0a960ffc164965f5
b20083 <e1b20083@st.oit.ac.jp>
2022-10-11 14:52:39 +0900

    T7

---
+++ src/main/java/oit/is/lec03/lec03/controller/Sample31Controller.java
@@ -0,0 +1,15 @@
+package oit.is.lec03.lec03.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("sample3")
+public class Sample31Controller {
+  @GetMapping("step1")
+  public String sample31() {
+    return "sample31.html";
+  }
+
+}
