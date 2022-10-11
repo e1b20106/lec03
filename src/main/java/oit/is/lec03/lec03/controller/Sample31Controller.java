344383cdeb36f56cb7e63cdf9e53d50429719452
e1b20106 <e1b20106@st.oit.ac.jp>
2022-10-11 16:10:50 +0900

    sample3-2の実装

--- src/main/java/oit/is/lec03/lec03/controller/Sample31Controller.java
+++ src/main/java/oit/is/lec03/lec03/controller/Sample31Controller.java
@@ -1,6 +1,9 @@
 package oit.is.lec03.lec03.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +12,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class Sample31Controller {
   @GetMapping("step1")
   public String sample31() {
+    return "sample31.html";
+  }
+
+  @GetMapping("step2")
+  public String sample32(ModelMap model, Principal prin) {
+    String loginUser = prin.getName(); // ログインユーザ情報
+    model.addAttribute("login_user", loginUser);
     return "sample31.html";
   }
 
